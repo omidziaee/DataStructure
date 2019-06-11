1db03823a905c3e3950131aab4aa61665c638f65
omid <omid@192.168.2.5>
2019-06-11 10:07:51 -0700

    Added from my mac

--- Algorithm_And_Data_Structure/Test/Test.py
+++ Algorithm_And_Data_Structure/Test/Test.py
@@ -1,21 +1,24 @@
 class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        ans = []
-        combination = []
-        used = []
-        self.to_find_all_perm(nums, ans, combination)
-        return ans
-    def to_find_all_perm(self, nums, ans, combination):
-        if len(combination) == len(nums):
-            ans.append(combination)
-        for i in range(len(nums)):
-            if nums[i] in combination:
-                continue
-            self.to_find_all_perm(nums, ans, combination + [nums[i]])
-        return ans
+    def IDsOfSongs(self, rideDuration, songDurations):
+        # WRITE YOUR CODE HERE
+        if not songDurations or len(songDurations) == 0:
+            return []
+        target = rideDuration - 30
+        dic_index_duration = {}
+        res_index = []
+        res_duration = []
+        for i, duration in enumerate(songDurations):
+            if target - duration in dic_index_duration:
+                res_index.append([dic_index_duration[target - duration], i])
+                res_duration.append([target - duration, duration])
+            else:
+                dic_index_duration[duration] = i
+        max_length = 0
+        candid = []
+        for i in range(len(res_duration)):
+            if max(res_duration[i]) > max_length:
+                max_length = max(res_duration[i])
+                candid = res_index[i]
+        return candid
 sol = Solution()
-print sol.permute([1,2,3])
+print sol.IDsOfSongs(110, [])
