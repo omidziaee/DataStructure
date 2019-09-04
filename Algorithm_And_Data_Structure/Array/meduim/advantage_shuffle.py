d0edca3b7a46b5e07a4624a580e8f9ad8fa11e23
omid.ziaee@us.abb.com <omid.ziaee@us.abb.com>
2019-09-03 21:41:12 -0700

    Added from ABB

---
+++ Algorithm_And_Data_Structure/Array/meduim/advantage_shuffle.py
@@ -0,0 +1,39 @@
+'''
+Created on Sep 3, 2019
+
+@author: USOMZIA
+Given two arrays A and B of equal size, the advantage of A with respect to B is
+the number of indices i for which A[i] > B[i].
+Return any permutation of A that maximizes its advantage with respect to B.
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+Example 2:
+
+Input: A = [12,24,8,32,24], B = [13,25,32,11,11]
+Output: [24,32,8,12]
+'''
+class Solution(object):
+    def advantageCount(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: List[int]
+        """
+        import bisect
+        ans = []
+        A.sort()
+        for num in B:
+            indx = bisect.bisect_left(A, num)
+            if indx == len(A):
+                ans.append(A.pop(0))
+            else:
+                ans.append(A.pop(indx))
+        return ans
+    
+A = [12,24,8,32,24]
+B = [13,25,32,11,11]
+sol = Solution()
+print sol.advantageCount(A, B)
