c6aea24c29278bbea489ae287f24c84724dbfb6f
omid <omid@192.168.2.15>
2019-09-27 10:26:49 -0700

    Added from my mac

--- Algorithm_And_Data_Structure/Array/meduim/sub_array_divisible_by_k.py
+++ Algorithm_And_Data_Structure/Array/meduim/sub_array_divisible_by_k.py
@@ -24,12 +24,24 @@ class Solution(object):
         dic_cum_sum = {}
         cum_sum = 0
         counter = 0
+        # This one is necessary because at least we know that for each value the value minus that value is zero
+        dic_cum_sum[0] = 1
+        # We should use just one pass and fill both hashmap and check for the needed value at the same loop
+        # similar to sum of two numbers if we do it in two separate loop and we have one value and the twice of it
+        # we will get larger number
         for i in range(len(A)):
             cum_sum += A[i]
-            dic_cum_sum[i] = cum_sum
-        max_div = cum_sum / K
-        for i in range(max_div):
-            if dic_cum_sum - K * i in dic_cum_sum:
-                counter += 1
+            needed = cum_sum % K
+            if needed in dic_cum_sum:
+                counter +=dic_cum_sum[needed]
+                dic_cum_sum[needed] += 1
+            else:
+                dic_cum_sum[needed] = 1
+        return counter
+
+A = [4,5,0,-2,-3,1]
+K = 5
+sol = Solution()
+print sol.subarraysDivByK(A, K)
                     
         
