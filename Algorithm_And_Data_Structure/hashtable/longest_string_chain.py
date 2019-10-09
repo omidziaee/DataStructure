8358b1bf35432064ecd3b184b5e59d998c6d2af7
omid.ziaee@us.abb.com <omid.ziaee@us.abb.com>
2019-10-08 20:40:54 -0700

    Added from ABB laptop

--- Algorithm_And_Data_Structure/hashtable/longest_string_chain.py
+++ Algorithm_And_Data_Structure/hashtable/longest_string_chain.py
@@ -20,3 +20,32 @@ Input: ["a","b","ba","bca","bda","bdca"]
 Output: 4
 Explanation: one of the longest word chain is "a","ba","bda","bdca".
 '''
+class Solution():
+    def longestStrChain(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        import collections
+        if len(words) < 2:
+            return len(words)
+        max_chain = 0
+        words.sort(key = lambda w:len(w))
+        for i in range(len(words)):
+            first_counter = collections.Counter(words[i])
+            counter = 1
+            for j in range(i + 1, len(words)):
+                counter_chars = 0
+                for key in first_counter.keys():
+                    if key in collections.Counter(words[j]):
+                        counter_chars += 1
+                if counter_chars == len(first_counter):
+                    counter += 1
+                    first_counter = collections.Counter(words[j])
+            max_chain = max(max_chain, counter)
+        return max_chain 
+                
+words = ["ddgpj","oopwqq","ooq","oopq","iwdkeoqqtd","iwdkeoqqt","oopwq","t","wdoqqt","vcw","ddgpjy","ddpj","njpci","njci","ft","q","wdkeoqqt","dqq","qq","ni","eihk","ebiihzke","eihzke","eik","eiihzke","dqqt","eihzk","vw","ddp","oq","wdeoqqt","nci","doqqt","vft"]
+sol = Solution();
+print sol.longestStrChain(words)
+                
